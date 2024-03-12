050fa289f4fd8ae4cd8bd717fff6711306d2d944
OmarrHamdan <oma20191090@std.psut.edu.jo>
2024-03-12 15:13:53 +0000

    g

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LogoutButton from "../components/LogoutButton"; // replace with the actual path to LogoutButton
+import './HomePage.css';
+
 
 const HomePage = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src="src\assets\Capture.PNG" alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/add-recipe">Submit New Recipe</Link>
+          </li>
+          <li>
+            <Link to="/add-ingredients">Submit Recipe Ingredients</Link>
+          </li>
+          <li>
+            <Link to="/my-recipes">View My Recipes</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default HomePage;
+
+
+/*const HomePage = () => {
   return (
     <div>
       <h1>DishConnect</h1>
@@ -17,4 +45,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default HomePage;*/
