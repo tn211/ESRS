4eefed073c2f63371266f403e34c88b2d1615b58
OmarrHamdan <oma20191090@std.psut.edu.jo>
2024-03-13 13:16:10 +0000

    Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,46 +3,39 @@ import { Link } from "react-router-dom";
 import LogoutButton from "../components/LogoutButton"; // replace with the actual path to LogoutButton
 import './HomePage.css';
 
-
 const HomePage = () => {
   return (
-    <header>
-      <div className="logo">
-        <img src="src\assets\Capture.PNG" alt="Logo" />
-      </div>
+    <><header>
       <nav>
-        <ul>
+        <ul className="menu">
           <li>
-            <Link to="/add-recipe">Submit New Recipe</Link>
+            <Link to="/add-recipe">Page 1</Link>
           </li>
           <li>
-            <Link to="/add-ingredients">Submit Recipe Ingredients</Link>
+            <Link to="/add-ingredients">Page 2</Link>
           </li>
           <li>
-            <Link to="/my-recipes">View My Recipes</Link>
+            <Link to="/my-recipes">Page 3</Link>
+          </li>
+          <li>
+            <Link to="/my-recipes">Page 4</Link>
+          </li>
+          <li>
+            <Link to="/my-recipes">Page 5</Link>
           </li>
         </ul>
       </nav>
+      <div className="logo-container">
+        <a href="/">
+          <img src="src\assets\Dishconnect.PNG" alt="Logo" className="logo" />
+        </a>
+      </div>
     </header>
+      <footer>
+        <p>FOOTER</p>
+      </footer></>
   );
+
 };
 
 export default HomePage;
-
-
-/*const HomePage = () => {
-  return (
-    <div>
-      <h1>DishConnect</h1>
-      <Link to="/add-recipe">Submit New Recipe</Link>
-      <br />
-      <Link to="/add-ingredients">Submit Recipe Ingredients</Link>
-      <br />
-      <Link to="/my-recipes">View My Recipes</Link>
-      <br />
-      <LogoutButton />
-    </div>
-  );
-};
-
-export default HomePage;*/
