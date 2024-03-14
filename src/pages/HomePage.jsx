e778bdf9037b5b24320ec3bfc05dbe49d0062ea0
OmarrHamdan <oma20191090@std.psut.edu.jo>
2024-03-14 16:24:05 +0000

    Omar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LogoutButton from "../components/LogoutButton"; // replace with the actual path to LogoutButton
 import './HomePage.css';
+import { HiUserCircle } from "react-icons/hi2";
+const divStyle = {
 
+  color: 'black',
+
+
+
+};
 const HomePage = () => {
   return (
     <><header>
+      <div className="icon-container">
+        <Link to="/add-recipe"> <HiUserCircle className="icon" /></Link>
+      </div>
       <nav>
         <ul className="menu">
           <li>
@@ -31,10 +41,38 @@ const HomePage = () => {
         </a>
       </div>
     </header>
+      <div className="image-grid">
+        <div className="image-item">
+          <img src="src\assets\FOOD.jpg" alt="Image 1" />
+          <p style={divStyle}>Text underneath image 1</p>
+        </div>
+        <div className="image-item">
+          <img src="src\assets\FOOD.jpg" alt="Image 2" />
+          <p style={divStyle}>Text underneath image 2</p>
+        </div>
+        <div className="image-item">
+          <img src="src\assets\FOOD.jpg" alt="Image 3" />
+          <p style={divStyle}>Text underneath image 3</p>
+        </div>
+        <div className="image-item">
+          <img src="src\assets\FOOD.jpg" alt="Image 4" />
+          <p style={divStyle}>Text underneath image 4</p>
+        </div>
+        <div className="image-item">
+          <img src="src\assets\FOOD.jpg" alt="Image 5" />
+          <p style={divStyle}>Text underneath image 5</p>
+        </div>
+        <div className="image-item">
+          <img src="src\assets\FOOD.jpg" alt="Image 6" />
+          <p style={divStyle}>Text underneath image 6</p>
+        </div>
+      </div>
       <footer>
-        <p>FOOTER</p>
+        <p>DishConnect © 2024. All rights reserved</p>
       </footer></>
+
   );
+
 
 };
 
