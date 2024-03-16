0284ff34d95ac0449bddbd06990a37b2a6afe168
OmarrHamdan <oma20191090@std.psut.edu.jo>
2024-03-16 00:32:56 +0000

    responsive menu?

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import LogoutButton from "../components/LogoutButton"; // replace with the actual path to LogoutButton
 import { HiUserCircle } from "react-icons/hi2";
 import './Layout.css';
 
 const Layout = ({ children }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
 
-        <><header>
+        <><header className={`header ${menuOpen ? "menu-open" : ""}`}>
             <div className="icon-container">
                 <Link to="/account"> <HiUserCircle className="icon" /></Link>
             </div>
+            <div className="menu-toggle" onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
             <nav>
-                <ul className="menu">
+                <ul className={`menu ${menuOpen ? "open" : ""}`}>
                     <li>
                         <Link to="/add-recipe">Add Recipe</Link>
                     </li>
