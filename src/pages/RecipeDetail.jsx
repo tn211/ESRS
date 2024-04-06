c8c0f87f17a5bca2c7b8e783faac6628ac2f69e6
tn211 <72459042+tn211@users.noreply.github.com>
2024-04-06 17:36:42 +0100

    first attempts with Disqus

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom'; // Ensure Link is imported
 
 const RecipeDetail = ({ supabase }) => {
   const { recipeId } = useParams();
@@ -32,6 +32,32 @@ const RecipeDetail = ({ supabase }) => {
     fetchRecipeDetail();
   }, [recipeId, supabase]);
 
+  useEffect(() => {
+    window.disqus_config = function () {
+      this.page.url = window.location.href;
+      this.page.identifier = recipeId;
+    };
+
+    const disqusScriptId = 'disqus-script';
+    let script = document.getElementById(disqusScriptId);
+    if (!script) {
+      script = document.createElement('script');
+      script.id = disqusScriptId;
+      script.src = 'https://dishconnect.disqus.com/embed.js';
+      script.setAttribute('data-timestamp', +new Date());
+      document.body.appendChild(script);
+    } else {
+      window.DISQUS.reset({
+        reload: true,
+        config: function () {
+          this.page.url = window.location.href;
+          this.page.identifier = recipeId;
+        },
+      });
+    }
+
+  }, [recipeId]); 
+
   if (!recipe) {
     return <div>Loading...</div>;
   }
@@ -55,6 +81,8 @@ const RecipeDetail = ({ supabase }) => {
         <Link to="/recent-recipes">Recently Added</Link>
         <br />
         <Link to="/">Back to Home</Link>
+        <div id="disqus_thread"></div>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
       </div>
     </>
   );
