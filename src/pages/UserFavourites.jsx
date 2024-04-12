0e33e7af84c26af5634e0e2b66936f8e5ce12a2b
Matt <matstockdale@gmail.com>
2024-04-12 12:17:50 +0100

    favourites page

---
+++ src/pages/UserFavourites.jsx
@@ -0,0 +1,16 @@
+import Layout from "./Layout";
+
+
+function UserFavouritesPage() {
+
+    return (
+        <>
+            <Layout>
+                <h1>TESTING FAVOURITES PAGE</h1>
+            </Layout>
+            
+        </>
+    );
+}
+
+export default UserFavouritesPage;
