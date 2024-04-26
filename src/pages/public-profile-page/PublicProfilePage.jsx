554e13b90a44006bee6542552d99ace24ae377c2
Matt <matstockdale@gmail.com>
2024-04-26 19:52:54 +0100

    move profile page

--- src/pages/public-profile-page/PublicProfilePage.jsx
+++ src/pages/public-profile-page/PublicProfilePage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
-import Layout from './Layout';
-import RecipesList from '../components/RecipeList';
+import { supabase } from '../../supabaseClient';
+import Layout from '../Layout';
+import RecipesList from '../../components/RecipeList';
 import './PublicProfilePage.css';
 
 const PublicProfilePage = ({ session }) => {
