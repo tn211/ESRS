b6aefbd3a3004b370236bd1cb8f7cba74472b337
OmarrHamdan <oma20191090@std.psut.edu.jo>
2024-04-26 22:55:27 +0100

    12

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { supabase } from "./supabaseClient";
 import Auth from "./Auth";
-import Account from "./pages/Account";
-import HomePage from "./pages/HomePage";
-import RecipeEntryPage from "./pages/RecipeEntryPage";
-import UserRecipesPage from "./pages/UserRecipesPage";
-import RecipeDetail from "./pages/RecipeDetail";
-import AboutUs from "./pages/AboutUs";
-import RecentRecipesPage from "./pages/RecentRecipesPage";
+import Account from "./pages/account-page/Account";
+import HomePage from "./pages/home-page/HomePage";
+import RecipeEntryPage from "./pages/recipe-entry-page/RecipeEntryPage";
+import UserRecipesPage from "./pages/my-recipes-page/UserRecipesPage";
+import RecipeDetail from "./pages/recipe-detail-page/RecipeDetail";
+import AboutUs from "./pages/about-us-page/AboutUs";
+import RecentRecipesPage from "./pages/community-page/RecentRecipesPage";
 import SearchPage from "./pages/search-page/SearchPage";
-import UserFavouritesPage from "./pages/UserFavourites";
-import PublicProfilePage from "./pages/PublicProfilePage";
-import FollowingPage from "./pages/FollowingPage";
+import UserFavouritesPage from "./pages/favorites-page/UserFavourites";
+import PublicProfilePage from "./pages/public-profile-page/PublicProfilePage";
+import FollowingPage from "./pages/following-page/FollowingPage";
 
 function App() {
   const [session, setSession] = useState(null);
