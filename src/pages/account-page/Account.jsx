57d34822404471b69443133a4ef5d58749c315ef
Matt <matstockdale@gmail.com>
2024-04-26 19:56:17 +0100

    move account

--- src/pages/account-page/Account.jsx
+++ src/pages/account-page/Account.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../../supabaseClient'
 import { useNavigate } from "react-router-dom"; 
-import Avatar from '../components/Avatar'
+import Avatar from '../../components/Avatar'
 import './Account.css';
-import Layout2 from "./Layout2"; 
-import Layout from "./Layout"; 
+import Layout2 from "../Layout2"; 
+import Layout from "../Layout"; 
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true)
