ca9a909a75120dde7f891d0cc24f76438d97fe5f
Matt <matstockdale@gmail.com>
2024-04-26 14:08:16 +0100

    profile images displaying

--- src/pages/PublicProfilePage.jsx
+++ src/pages/PublicProfilePage.jsx
@@ -9,6 +9,7 @@ const PublicProfilePage = () => {
   const { id } = useParams(); // This is the profile_id from the URL
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [avatarUrl, setAvatarUrl] = useState('');
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -16,7 +17,7 @@ const PublicProfilePage = () => {
 
       const { data: userData, error: userError } = await supabase
         .from('profiles')
-        .select('username')
+        .select('username, avatar_url')
         .eq('id', id)
         .single();
 
@@ -28,24 +29,40 @@ const PublicProfilePage = () => {
 
       setUser(userData);
       setLoading(false);
+      setAvatarUrl(userData.avatar_url); // Set the avatar URL
     };
 
     fetchUserProfile();
   }, [id, supabase]);
 
+  const getFullImageUrl = (imagePath) => {
+    const baseUrl = 'https://nwooccvnjqofbuqftrep.supabase.co/storage/v1/object/public/avatars';
+    return `${baseUrl}/${imagePath}`;
+  };
+
   return (
     <Layout>
       <div className='public-profile-page'>
-        <h1>{user ? `${user.username}'s Recipes` : "Loading user..."}</h1>
-        {!loading ? (
-          <RecipesList supabase={supabase} userId={id} /> // Pass userId to RecipesList
-        ) : (
-          <p>Loading recipes...</p>
+        {user && (
+          <>
+            <h1>{`${user.username}'s Recipes`}</h1>
+            <div className='profile-img-wrapper'>
+              <img src={avatarUrl ? getFullImageUrl(avatarUrl) : "/src/assets/placeholder.png"} alt={`${user.username}'s profile`} style={{ maxWidth: '100%' }} />
+            </div>
+            {!loading ? (
+              <RecipesList supabase={supabase} userId={id} /> // Pass userId to RecipesList
+            ) : (
+              <p>Loading recipes...</p>
+            )}
+          </>
         )}
+        {loading && <p>Loading user...</p>}
       </div>
     </Layout>
   );
 };
 
 export default PublicProfilePage;
+
+
 
