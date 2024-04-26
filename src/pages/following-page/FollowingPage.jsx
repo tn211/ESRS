47c9ba3808746789eb0682135d58c01a0cbd4e2d
Matt <matstockdale@gmail.com>
2024-04-26 19:56:53 +0100

    move following

--- src/pages/following-page/FollowingPage.jsx
+++ src/pages/following-page/FollowingPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
-import Layout from './Layout';
+import { supabase } from '../../supabaseClient';
+import Layout from '../Layout';
 import './FollowingPage.css';
 
 const FollowingPage = ({ session }) => {
