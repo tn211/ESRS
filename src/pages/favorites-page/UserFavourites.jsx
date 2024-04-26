fc5843079db4062e79132edd0047d1208b083edd
Matt <matstockdale@gmail.com>
2024-04-26 20:00:55 +0100

    move favs

--- src/pages/favorites-page/UserFavourites.jsx
+++ src/pages/favorites-page/UserFavourites.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; // Ensure you import Link from react-router-dom
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 import Layout from './Layout';
 
 const UserFavouritesPage = ({ session }) => {
