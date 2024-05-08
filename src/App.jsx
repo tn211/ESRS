9fef4f2f5ad4e5c2dd1eb428876d788bb9c4c657
tn211 <72459042+tn211@users.noreply.github.com>
2024-05-08 10:11:23 +0100

    removed global styling

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   }, []);
 
   return (
-    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+    <div className="container">
       {!session ? (
         <Auth />
       ) : (
