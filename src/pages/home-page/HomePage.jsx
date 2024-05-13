78cdeef52757da40a9821bb28a54c0edcdeeddf5
Bravea16 <158195253+Bravea16@users.noreply.github.com>
2024-05-13 20:35:38 +0100

    Update HomePage.jsx

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -2,37 +2,6 @@ import React, { useState, useEffect } from "react";
 import './HomePage.css';
 import Layout2 from "../../components/layout-components/Layout2";
 
-const images = [
-    "src/assets/food1.jpg",  // update path
-    "src/assets/food2.jpg",
-    "src/assets/food3.jpg"
-];
-
-const Carousel = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentIndex((currentIndex + 1) % images.length);
-        }, 4000); // Cambia imagen cada 3 segundos
-
-        return () => clearInterval(intervalId);
-    }, [currentIndex]);
-
-    return (
-        <div className="carousel">
-            {images.map((image, index) => (
-                <div
-                    key={index}
-                    className="carousel-item"
-                    style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-                >
-                    <img src={image} alt={`Slide ${index}`} />
-                </div>
-            ))}
-        </div>
-    );
-};
 
 const HomePage = () => {
   return (
